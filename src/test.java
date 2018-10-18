1b1d9a8759809fde82bdeb6d1889690f3a208ca6
pwellener19 <pwellener19@HS-MATH2-29.gvsd.org>
2018-10-18 14:24:42 -0400

    Yos

--- src/test.java
+++ src/test.java
@@ -10,18 +10,33 @@ public class test
 		boolean tryAgain = true;
 		while (tryAgain)
 		{
-		System.out.println("Which option would you like to run: \nOption 1: Replaces all the vowels with  *\nOption 2: Reverses the string\nWhich option do  you choose(in numerical represenation)");
+		boolean validNum = true;
+		while (validNum)
+		{
+		System.out.print("Which option would you like to run: \nOption 1: Replaces all the vowels with  *\nOption 2: Reverses the string\nWhich option do  you choose(in numerical represenation): ");
 		int option = in.nextInt();
-		System.out.println("What string would you like to use");
+		System.out.print("What string would you like to use: ");
 		String str1 = in.next();
 		if (option == 1)
 		{
 			System.out.println(manip.noVowels(str1));
+			validNum = false;
 		}
-		if (option == 2)
+		else if (option == 2)
 		{
-			System.out.println(manip.reverse(str1));	
+			System.out.println(manip.reverse(str1));
+			validNum = false;
 		}
+		else
+			System.out.println("This is not a valid. Try again");
+			validNum = true;
+		}
+		System.out.print("If you like to play again, press 1. If you would like to end the program, press 2: ");
+		int again = in.nextInt();
+		if (again == 1)
+			tryAgain = true;
+		else if (again == 2)
+			tryAgain = false;
 		}
 	}
 
